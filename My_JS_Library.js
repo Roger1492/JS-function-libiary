92e6cd6e0ca9096f19cbcfcf537309ce34ef3f55
Roger1492 <rogershen1492@gmail.com>
2018-08-05 23:36:35 +0800

    update

--- My_JS_Library.js
+++ My_JS_Library.js
@@ -2,8 +2,8 @@ function JT() { };
 let _ = new JT();
 
 /////////////////////////////
-///////// 数组方法
-/////////////////////////////
+///////// 数组方法 //////////
+////////////////////////////
 
 // 返回数组中的第一个元素
 JT.prototype.first = function (array) {
@@ -16,11 +16,26 @@ JT.prototype.last = function (array) {
 }
 
 // 返回数组所有元素之和
-JT.prototype.sum = function(array){
-    return array.reduce((a,b) => a+b);
+JT.prototype.sum = function (array) {
+    return array.reduce((a, b) => a + b);
 }
 
 // 返回数组所有元素之积
-JT.prototype.times = function(array){
-    return array.reduce((a,b) => a*10+b);
+JT.prototype.times = function (array) {
+    return array.reduce((a, b) => a * 10 + b);
 }
+
+// 生成指定范围内的数字
+JT.prototype.range = function (start, stop, step) {
+
+    let arr = [];
+
+    if (step === undefined || step === 1) {
+        step = 1;
+    }
+
+    for (let i = start; i < stop; i += step) {
+        arr.push(i);
+    }
+    return arr;
+};
