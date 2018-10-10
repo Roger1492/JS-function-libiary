a3a40be54bab63123d2674aad16112feca978f13
Roger1492 <rogershen1492@gmail.com>
2018-10-10 16:40:54 +0800

    update sortQuick.js use the new way that I can understand

--- Sort/sortQuick.js
+++ Sort/sortQuick.js
@@ -1,43 +1,29 @@
 /**
  * @description 排序算法 - 快速排序
+ * @access http://www.ruanyifeng.com/blog/2011/04/quicksort_in_javascript.html
  * @param {Array}
  * @return {Array}
  */
 
-function sortQuick(arr, left, right){
-    var len = arr.length,
-        partitionIndex,
-        left = typeof left != "number" ? 0 : left,
-        right = typeof right != "number" ? len - 1 : right;
+function sortQuick(arr){
+    if(arr.length <= 1) {return arr;}
 
-    if(left < right){
-        partitionIndex = partition(arr, left, right);
-        sortQuick(arr, left, partitionIndex - 1);
-        sortQuick(arr, partitionIndex+1, right);
-    }
-    return arr;
-}
+    let pivotIndex = Math.floor(arr.length / 2);
+    let pivot = arr.splice(pivotIndex,1)[0];
+    let left = [];
+    let right= [];
 
-function partition(arr, left, right){       // 分区操作
-    let pivot = left,                       // 设定基准值（pivot）
-        index = pivot + 1;
-    for(let i = index; i <= right; i++){
-        if(arr[i] < arr[pivot]){
-            swap(arr, i, index);
-            index++;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] < pivot){
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
         }
     }
-    swap(arr, pivot, index-1);
-    return index - 1;
-}
 
-function swap(arr, i, j){
-    let temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
+    return sortQuick(left).concat(pivot, sortQuick(right));
 }
-
 
 // 示例
-let a = [16, 11, 3, 6, 10, 1, 2, 8, 5, 6, 4];
+let a =  [39, 99, 299, 187, 65, 23, 12, 76, 90, 142, 43, 87, 51, 6, 3];
 console.log(sortQuick(a));
